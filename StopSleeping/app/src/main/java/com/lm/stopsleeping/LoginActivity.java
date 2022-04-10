7a3ed8bfdd5554c4bd8046a1c5dbf6fe4e81dfde
wonchan9999 <wonchan999@naver.com>
2022-04-10 16:15:25 +0900

    wonchanTest1

--- StopSleeping/app/src/main/java/com/lm/stopsleeping/LoginActivity.java
+++ StopSleeping/app/src/main/java/com/lm/stopsleeping/LoginActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
     private TextView btn_register;
+    private Button btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +18,24 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         btn_register = findViewById(R.id.lg_register_btn);
+        btn_login = findViewById(R.id.lg_login_btn);
+
 
         // 회원가입 버튼 클릭시 회원가입 페이지로 이동
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent =  new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        // 로그인 버튼 클릭시 테스트 페이지로 이동
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =  new Intent(LoginActivity.this, TestActivity.class);
                 startActivity(intent);
             }
         });
